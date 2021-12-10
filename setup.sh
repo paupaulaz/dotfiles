dd5b3fc02d4ea273f1bb6a649bb776223ef4aaf6
Paul Azoulai <pazoulai@gmail.com>
2021-12-10 11:06:43 -0500

    wip

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# all machines
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.bashrc ~/.bashrc
+touch ~/.bashrc.local
+ln -s `pwd`/.gf ~/.gf
+
+# linux specific
+
+
+
+# macos specific
+
